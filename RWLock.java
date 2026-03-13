dde8957eabf536c55b472287e30f94b4bad071b1
fetyandaoud <167618556+fetyandaoud@users.noreply.github.com>
2026-03-13 01:06:10 +0100

    Add files via upload

---
+++ RWLock.java
@@ -0,0 +1,25 @@
+package readerwriter;
+
+public class RWLock {
+
+    public RWLock(){
+
+    }
+
+    public void acquireRead(){
+
+    }
+
+    public void acquireWrite(){
+
+    }
+
+    public void releaseRead(){
+
+    }
+
+    public void releaseWrite(){
+
+    }
+
+}
